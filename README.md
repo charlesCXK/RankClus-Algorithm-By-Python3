caea5d45858e2da409e2e3e3c924bf52d7d042d7
charlesCXK <715811763@qq.com>
2018-07-20 14:37:49 +0800

    update

---
+++ README.md
@@ -0,0 +1,204 @@
+# 2018 PKU Data Warehouse
+
+### 一、文件与编译使用说明
+
+#### 1、数据文件
+
+- **data** 文件夹：包含了需要的xml数据文件以及提取后的文件
+
+  **dblp.xml** 在github中没有上传，请自行下载<a href = "http://dblp.org/xml/release/">dblp-2018-05-01</a>）
+
+
+- **output** 文件夹：存放了对$author、council$排名的（每个类别取前十位）结果（CSV格式）
+
+
+- **pic **文件夹：存放了相关图片
+
+#### 2、代码文件
+
+- **HandleXML.ipynb**：预处理xml文件，从中提取出信息
+
+
+- **simple.ipynb** && **authority.ipynb**：分别用 SimpleRanking 和 AuthorityRanking 排序方法实现的RankClus算法。
+
+
+- **multi_simple.ipynb** && **multi_authority.ipynb**：上述代码的对应的多进程版本
+- **RankClus.py**：多线程版本、使用AuthorityRanking的RankClus算法
+
+#### 3、运行环境 && 运行方法
+
+- 编写时运行环境：$macOS\ Sierra\ 10.12,\ \ \ 2.6\ GHz\ Intel\ Core\ i7,\ \ \ 16GB\ DDR3,\ \ \ 4\ cores$
+- 编程语言 && 工具：$python3$ && $jupyter\ notebook$
+- 运行步骤
+
+**第一种方法**：用**jupyter notebook**运行
+
+（1）用命令行打开 jupyter notebook
+
+```perl
+$ ipython notebook
+```
+
+（2）选择需要运行的ipynb文件。点击上方菜单栏 $=> Cell => Run All$。
+
+**第二种方法**：运行 .py 文件
+
+```perl
+$ python3 RankClus.py
+```
+
+**命令行参数**
+
+~~~Python
+usage: RankClus.py [-h] [-k CLUSTERNUM] [-i ITERNUM] [-e EMNUM] [-a ALPHA]
+                   [-r RANK]
+
+optional arguments:
+  -h, --help     show this help message and exit		# 显示帮助
+  -k CLUSTERNUM  Number of total clusters, default=15		# RankClus算法的总类别数目
+  -i ITERNUM     Number of iterations of RankClus, default=30		# RankClus算法迭代次数
+  -e EMNUM       Number of iterations of EM algorithm, default=5	# 混合模型中EM算法迭代次数
+  -a ALPHA       Parameter alpha, used in rank function, default=0.95	# 参数 alpha
+  -r RANK        Choose a rank function, 0:SimpleRanking and		
+                 1:AuthorityRanking, default=1				# 选择排序算法，1代表权威排序，0代表简单排序
+~~~
+
+<br>
+
+<br>
+
+<br>
+
+### 二、程序流程
+
+#### 1、对XML文件的预处理：HandleXML.ipynb
+
+**包括**：
+
+- 将其中的类似'&ouml ;'的字符串删掉，这是unicode遗留问题
+- 提取出1998-2007年间的（council、author）信息，写入到新文件 data/data.txt。每行的格式为：会议$作者1;作者2;.....作者n;
+
+
+<br>
+
+
+#### 2、RankCLus算法
+
+#### Step 0: Initialization.
+
+第一步，初始化。将会议随机分配到K个类别中。Project中 K 设定为15.
+
+------
+
+#### Step 1: Ranking for each cluster
+
+在每个类别中，计算作者与会议的条件排名。排名方法有**Simple Ranking**和**Authority Ranking**两种
+
+##### **————Simple Ranking**————
+
+计算公式如下图:
+
+<div align="center"><img src="pic/1.jpg" width="400"/></div>
+
+其中$W_{XY}(i, j)$表示会议$i$与作者$j$之间的链接数。$\vec{r}_{X}(x)$和$\vec{r}_{Y}(y)$分别是会议和作者在这个分类情况下的排名。
+
+
+
+##### **————Authority Ranking————**
+
+**Authority Ranking**的原理是基于这样一个直觉：**高排名的作者倾向于向高排名的会议投稿，高排名的会议更容易吸引高排名的人**。所以$$\vec{r}_{X}(x)$$和$$\vec{r}_{Y}(y)$$其实是相互影响的。下图是会议、作者排名的计算公式(包含归一化的步骤)，$W_{YX}(i, j)$表示作者$i$与会议$j$之间的链接数
+
+<div align="center"><img src="pic/2.jpg" width="400"/></div>
+
+同时我们也认为，**作者之间的排名会相互影响**，比如说，相互之间有合作（共同发表论文）的作者会提高相互之间的排名。
+
+ <div align="center"><img src="pic/3.jpg" width="400"/></div>
+
+
+
+${aplha}$ 是自定义的一个参数，代表${作者—作者}$和${会议-作者}$之间影响权重。
+
+
+
+------
+
+#### Step 2: Estimation of the mixture model component co-efficients
+
+得到了现有类别中各个成员的排名之后，我们希望用这个排名重新估算某个类中某个成员属于类别 i 的概率。 i ∈ [1, K]。由于包含未知参数，EM算法显然派上了用场。假设上述提到的概率组成了一个矩阵Θ，现在我们想估计出这个矩阵的值。即：
+
+<div align="center"><img src='pic/4.jpg' width="400"></img></div>
+
+论文中经过一番推导，得出下图这样的公式。$π_{i,k}$即为会议$i$属于类别$K$的概率。
+
+<div align="center"><img src='pic/5.jpg' width="400"></img></div>
+
+$p_{k}(x_{i})$是给定类别$K$，会议$x_{i}$的排名。$p(x = k)$由如下公式计算：
+
+<div align="center"><img src='pic/6.jpg' width="400"></img></div>
+
+<div align="center"><img src='pic/7.jpg' width="400"></img></div>
+
+其中$W_{XY}(i, j)$是会议 i 到 作者 j 的链接数。经过几次迭代之后可以算出一个 Θ矩阵。
+
+------
+
+#### Step 3: Cluster adjustment
+
+Step2计算了Θ矩阵，包含了每个会议分别属于K个类别的概率。将这个K维向量作为这个会议的特征向量$\vec{S}_{x_{i}} = (\pi_{i, 1}, \pi_{i, 2}...\pi_{i, K} )$。这样每个类别可以用类别中所有会议的特征向量的均值算出一个聚类中心。接下来我们计算每个会议到K个类聚类中心的距离（用余弦相似度计算），找出距离最近的类，对会议 i 重新分配类别。
+
+------
+
+#### Step 4: Repeat Steps 1, 2 and 3
+
+迭代运行上述步骤。（Project代码中迭代次数设置为 30）
+
+<br>
+
+<br>
+
+<br>
+
+### 三、运行结果
+
+#### 4.1 使用AuthorityRanking排序算法输出的council信息（K=15）
+
+| 1                | 2         | 3           | 4              | 5           | 6         | 7                                | 8                                        | 9                                        | 10                           | 11         | 12                     | 13                | 14       | 15                                       |
+| ---------------- | --------- | ----------- | -------------- | ----------- | --------- | -------------------------------- | ---------------------------------------- | ---------------------------------------- | ---------------------------- | ---------- | ---------------------- | ----------------- | -------- | ---------------------------------------- |
+| HICSS            | NIPS      | DATE        | ICASSP         | INTERSPEECH | ICRA      | SODA                             | PVM/MPI                                  | KES                                      | Winter Simulation Conference | GLOBECOM   | CHI Extended Abstracts | VLDB              | IPDPS    | IGARSS                                   |
+| AMCIS            | ICML      | DAC         | ICME           | ICASSP (1)  | IROS      | SAC                              | SC                                       | CCCG                                     | ICSE                         | ICC        | CHI                    | ICDE              | Euro-Par | SMC                                      |
+| ECIS             | ICCV      | ICCAD       | EUSIPCO        | ICSLP       | ROBIO     | Computers and Their Applications | CCGRID                                   | KES (2)                                  | DG.O                         | WCNC       | UIST                   | SIGMOD Conference | ICPP     | International Conference on Computational Science (3) |
+| ICIS             | IJCAI     | ASP-DAC     | ICIP           | EUROSPEECH  | ISER      | ICDCS Workshops                  | HPDC                                     | KES (3)                                  | JISBD                        | PIMRC      | INTERACT               | CIKM              | GECCO    | ISNN (2)                                 |
+| PACIS            | UAI       | VLSI Design | ICIP (3)       | SSW         | Humanoids | AINA                             | CLUSTER                                  | KES (1)                                  | RE                           | INFOCOM    | JCDL                   | SIGIR             | PDPTA    | CCTA                                     |
+| HICSS (1)        | AAAI      | ITC         | ICIP (2)       | TSD         | RoboCup   | LCN                              | ICDCS                                    | IEA/AIE                                  | COMPSAC                      | VTC Spring | CSCW                   | EDBT              | PDP      | SMC (2)                                  |
+| Bled eConference | KDD       | ISLPED      | ICIP (1)       | ISCSLP      | ISRR      | CAINE                            | DSN                                      | IDEAL                                    | ICWS                         | VTC Fall   | IUI                    | PODS              | HiPC     | CIS                                      |
+| HICSS (6)        | CVPR (1)  | ICCD        | ICASSP (3)     | ASRU        | CIRA      | CAV                              | GRID                                     | ICTAI                                    | ECBS                         | AMIA       | UbiComp                | DASFAA            | LCPC     | SMC (4)                                  |
+| EGOV             | AAAI/IAAI | ISQED       | ACM Multimedia | IWSLT       | RO-MAN    | SEKE                             | International Conference on Computational Science | Australian Conference on Artificial Intelligence | WICSA                        | ICCCN      | AVI                    | TREC              | ICS      | ICIC (2)                                 |
+| IRMA Conference  | CVPR (2)  | VTS         | ICASSP (2)     | AVSP        | FSR       | AINA (1)                         | PPAM                                     | CIT                                      | OOPSLA Companion             | BROADNETS  | Graphics Interface     | DEXA              | HPCA     | SMC (1)                                  |
+
+<br>
+
+#### 4.2 不同排序算法之间的比较
+
+
+
+| 排序算法    | SimpleRanking | AuthorityRanking |
+| ------- | ------------- | ---------------- |
+| 单进程 (s) | 1216.839232   | 2074.304641      |
+| 多进程 (s) | 1248.64912    | 1458.097307      |
+
+| 排序算法       | SimpleRanking | AuthorityRanking |
+| ---------- | ------------- | ---------------- |
+| Rank (s)   | 78.58685756   | 945.9625461      |
+| EM (s)     | 1119.957891   | 1111.07937       |
+| Adjust (s) | 18.2673676    | 17.23624682      |
+
+<br><img src="pic/9.png" width="400"/>
+
+<br><img src="pic/10.png" width="400"/>
+
+<br><img src="pic/11.png" width="400"/>
+
+<br><img src="pic/12.png" width="400"/>
+
+<br>
+
---
+++ RankClus.py
@@ -0,0 +1,481 @@
+# coding: utf-8
+
+import random
+import heapq
+import time
+import argparse
+import multiprocessing
+import numpy as np
+
+from multiprocessing import Process
+from collections import defaultdict
+
+
+'''
+读入data文件，构建网络(作者-会议、会议-作者、作者-作者)
+parameters:
+    ---- C_A：会议-作者网络
+    ---- A_C：作者-会议网络
+    ---- A_A：作者=作者网络
+'''
+def BuildNet(filename):
+    con_dict = defaultdict(int)
+    tmp_aut_dict = defaultdict(int)
+    aut_dict = defaultdict(int)
+
+    with open('data/data.txt', 'r', encoding='utf-8') as f:
+        lines = f.readlines()
+
+    '''
+    统计所有会议的信息以及所有作者的信息。
+    '''
+    for line in lines:
+        line = line.strip()
+        conference = line.split('$')[0]    # 会议信息
+        authors = line.split('$')[1].split(';')[:-1]    # 作者(可能大于1)
+
+        con_dict[conference] = 1
+        for aut in authors:
+            tmp_aut_dict[aut] += 1    # 统计作者频率
+    # 取最大的 20000 个作者
+    for author in heapq.nlargest(20000, tmp_aut_dict.items(), lambda x: x[1]):
+        aut_dict[author[0]] = author[1]
+        
+    '''
+    构建三种字典
+    '''
+    C_A, A_C, A_A = {}, {}, {}
+    for key, value in con_dict.items():
+        C_A[key] = defaultdict(int)    # 作者-会议字典
+    for key, value in aut_dict.items():
+        A_A[key] = defaultdict(int)
+        A_C[key] = defaultdict(int)
+        
+    for line in lines:
+        line = line.strip()
+        conference = line.split('$')[0]    # 会议信息
+        authors = line.split('$')[1].split(';')[:-1]    # 作者(可能大于1)
+        
+        for aut in authors:
+            if aut not in aut_dict:    # 这个作者频率太低
+                continue
+            C_A[conference][aut] += 1
+            A_C[aut][conference] += 1
+            for aut2 in authors:
+                if aut2 not in aut_dict:    # 这个作者频率太低
+                    continue
+                if aut != aut2:
+                    A_A[aut][aut2] += 1
+                    A_A[aut2][aut] += 1
+    
+    return C_A, A_C, A_A
+
+
+'''
+Step 0: Initialization
+'''
+
+'''
+将会议初始化成K个类
+parameters
+    ---- CAnet：会议-作者网络
+    ---- K：需要划分的类=类别数
+'''
+def init_cluster(CAnet, K):
+    cluster = [[] for i in range(K)]    # 每个类一个列表
+    for k in CAnet.keys():
+        randnum = random.randint(0, K-1)
+        cluster[randnum].append(k)
+    return cluster
+
+
+'''
+Step 1: Ranking for each cluster
+'''
+
+'''
+实现 Simple Ranking
+parameters
+    ---- confer_author：会议-作者网络
+    ---- author_confer：作者-会议网络
+    ---- cluster：当前类别(包含了若干会议)
+'''
+def SimpleRanking(confer_author, author_confer, cluster):
+    #print("Ranking...")
+    rank_confer, rank_author, rank_allconfer = defaultdict(float), defaultdict(float), defaultdict(float)
+    
+    # 作者排名初始化
+    for author in author_confer:
+        rank_author[author] = 1.0 / len(author_confer)
+    # 统计会议和作者的排名
+    con_totsum = 0
+    aut_totsum = 0
+    for con in cluster:# 只计算这个类里的会议
+        rank_confer[con] = 0
+        for key, value in confer_author[con].items():    # 所有指向这个会议的作者求和
+            rank_confer[con] += value
+            con_totsum += value
+
+            rank_author[key] += value    # 作者在这个会议发表的论文数量
+            aut_totsum += value
+    for key, value in rank_confer.items():    # 对排名得分归一化
+        rank_confer[key] /= float(con_totsum)
+    for key, value in rank_author.items():    # 对排名得分归一化
+        rank_author[key] /= aut_totsum
+    
+    # 最后计算这个类别条件下，所有会议的rank。将会议排名再乘上作者排名————是否要归一化？经过输出发现，第一轮的时候貌似不同类别总和差的不是很大
+    golbal_sum = 0
+    for confer in confer_author:
+        for author,value in confer_author[confer].items():
+            rank_allconfer[confer] += value * rank_author[author]
+            golbal_sum +=  value * rank_author[author]
+    for confer in rank_allconfer:
+        rank_allconfer[confer] /= golbal_sum
+        
+    return rank_confer, rank_author, rank_allconfer
+
+'''
+实现 Authority Ranking
+parameters
+    ---- confer_author：会议-作者网络
+    ---- author_confer：作者-会议网络
+    ---- author_author：作者-作者网络
+    ---- cluster：当前类别
+    ---- alpha：人为定义的参数，在计算 author_rank 时衡量各部分权重
+'''
+def AuthorityRanking(confer_author, author_confer, author_author, cluster, alpha):
+    #print("Ranking...")
+    rank_confer, rank_author, rank_allconfer = defaultdict(float), defaultdict(float), defaultdict(float)
+    
+    # 记录类别中论文的数量以及所有作者的数量
+    tot_confer = len(cluster)
+    tot_author = len(author_confer)
+    # 对会议、作者排名进行初始化
+    for confer in cluster:
+        rank_confer[confer] = 1.0 / tot_confer
+    for author,value in author_confer.items():
+        rank_author[author] = 1.0 / tot_author
+    
+    # 循环更新 conference 和 author 的rank值
+    iternum = 0    # 迭代次数
+    
+    while iternum < 5:
+        iternum += 1    # 迭代次数
+        #print("iter num: {}".format(iternum))
+        
+        # --------先计算rank_confer--------
+        confer_sum = 0
+        newrank_confer = rank_confer.copy()
+        for confer in cluster:
+            rankc = 0
+            for author in confer_author[confer]:
+                rankc += confer_author[confer][author] * rank_author[author]
+            newrank_confer[confer] = rankc
+            confer_sum += rankc
+        
+        # confer排名归一化
+        for confer in rank_confer:
+            rank_confer[confer] = newrank_confer[confer] / confer_sum
+
+        # --------计算author_rank--------
+        save_rankc = rank_author.copy()    # 上一轮迭代的作者排名
+        author_sum = 0
+        
+        for author in save_rankc:
+            acsum = 0    # 作者与会议之间的影响
+            for confer in author_confer[author]:
+                acsum += author_confer[author][confer] * rank_confer[confer]
+            aasum = 0    # 作者之间相互影响
+            for a2 in author_author[author]:
+                aasum += author_author[author][a2] * save_rankc[a2]
+            rank_author[author] = alpha*acsum + (1-alpha)*aasum
+            author_sum += alpha*acsum + (1-alpha)*aasum
+        # author排名归一化
+        for author in rank_author:
+            rank_author[author] /= author_sum
+            
+
+        # 最后计算这个类别条件下，所有会议的rank。
+        golbal_sum = 0
+        for confer in confer_author:
+            for author in confer_author[confer]:
+                rank_allconfer[confer] += confer_author[confer][author] * rank_author[author]
+                golbal_sum +=  confer_author[confer][author] * rank_author[author]
+        for confer in rank_allconfer:
+            rank_allconfer[confer] /= golbal_sum
+        
+        
+    
+    return rank_confer, rank_author, rank_allconfer
+
+
+'''
+Step 2: Get new attributes for objects and cluster
+'''
+
+'''
+用EM算法估计参数
+parameters
+    ---- confer_author：会议-作者网络
+    ---- rank_confer：会议的排名
+    ---- rank_author：作者的排名
+    ---- cluster：所有的类别
+    ---- K：类别数
+    ---- iternum：EM算法迭代次数
+'''
+def EM(confer_author, rank_confer, rank_author, cluster, K, iternum=5):
+    pai = {}    # 每个会议有 K 维向量，代表属于第 i 个类别的概率
+    
+    '''
+    用每个类别的 confe-author 链接数来初始化 P(z=k)
+    '''
+    pz = [0 for k in range(K)]
+    tot_pz = 0
+    for i in range(K):
+        for confer in cluster[i]:    # 每个类别的会议
+            for author in confer_author[confer]:
+                pz[i] += confer_author[confer][author]
+    for ele in pz:
+        tot_pz += ele;
+    pz = [ele/tot_pz for ele in pz]
+
+    # 迭代更新
+    for iterk in range(iternum):
+        '''
+        更新 P(z=k)，通过  P(z=k | Xi,Yi,theta)
+        '''
+        print("——calculate P(z = k) ...")
+        # 计算 p(z = k|yj,xi,Θ0)
+        
+        pca = {}
+        
+        for confer in confer_author:
+            pca[confer] = {}
+            for author in confer_author[confer]:
+                pca[confer][author] = {}
+                sum_pca = 0    # 对每个类进行平均
+                for k in range(K):
+                    pca[confer][author][k] = rank_confer[k][confer] * rank_author[k][author] * pz[k]
+                    sum_pca += pca[confer][author][k]
+                for k in range(K):
+                    pca[confer][author][k] /= sum_pca
+                
+        new_pz = [0 for k in range(K)]
+
+        for k in range(K):
+            son = 0    # 分子
+            tot_pz = 0    # 用于求和归一化
+            for confer in confer_author:
+                for author in confer_author[confer]:
+                    Pz_condition = pca[confer][author][k]
+                    son += Pz_condition * confer_author[confer][author]    # 会议-作者权重 乘上 z=k 的条件概率
+                    tot_pz += confer_author[confer][author]
+            new_pz[k] = son / tot_pz
+        
+        for k in range(K):
+            pz[k] = new_pz[k]
+            
+    '''
+    计算 π(i,k)
+    '''
+    print("——calculate π ...")
+    for confer in confer_author:
+        pai[confer] = np.zeros(K)
+        sum_this_confer = 0.000001
+        for k in range(K):
+            pai[confer][k] = rank_confer[k][confer] * pz[k]
+            sum_this_confer += pai[confer][k]
+        # 归一化
+        pai[confer] /= sum_this_confer
+    return pai, pz
+
+
+'''
+Step 3: Adjust each object
+'''
+
+'''
+计算向量间距离 1-余弦相似度
+'''
+def myconsine(x, y):
+    dist = 1 - np.dot(x,y)/(np.linalg.norm(x)*np.linalg.norm(y))
+    return dist
+
+
+'''
+重新划分类别
+parameters
+    ---- confer_author：会议-作者网络
+    ---- cluster：所有类别
+    ---- feature：字典，保存所有会议的特征向量
+    ---- K：类别数
+'''
+def adjust_class(confer_author, cluster, feature, K):
+    '''
+    计算每个类的聚类中心
+    '''
+    S = [np.zeros(K) for k in range(K)]
+    
+
+    for i in range(K):
+        tot_sum = 0
+        for confer in cluster[i]:
+            S[i] += feature[confer]    # 这个会议的特征向量
+            tot_sum += 1
+        S[i] /= tot_sum    # 归一化
+    '''
+    根据聚类中心重新划分类别
+    '''
+    new_cluster = [[] for k in range(K)]
+    for confer in confer_author:
+        label = -1
+        now_min = 99999999
+        for k in range(K):
+            dist = myconsine(feature[confer], S[k])    # 会议 confer 跟第 k 个类别的距离
+            if dist < now_min:
+                now_min = dist
+                label = k
+        # 将这个会议分配到一个新的类别
+        new_cluster[label].append(confer)
+    return new_cluster
+
+'''
+判段是否有类别是空的，若有空的，返回 False
+'''
+def check_cluster(cluster):
+    for i in range(len(cluster)):
+        if len(cluster[i]) == 0:
+            return False
+    return True
+
+
+'''
+Main Function
+'''
+
+'''
+在多进程中调用的函数
+'''
+def poolrank(rank_confer, rank_author, k):
+    print("start ranking of cluster {}".format(k))
+    if RankAlgorithm == SimpleRanking:
+        tmp, rank_author[k], rank_confer[k] = RankAlgorithm(CAnet, ACnet, Cluster[k])
+    else:
+        tmp, rank_author[k], rank_confer[k] = RankAlgorithm(CAnet, ACnet, AAnet, Cluster[k], alpha)
+
+'''
+处理命令行参数
+'''
+def parse_arg():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-k', dest='clusternum', type=int, default=15, 
+        help='Number of total clusters, default=15')
+    parser.add_argument('-i', dest='iternum', type=int, default=30, 
+        help='Number of iterations of RankClus, default=30')
+    parser.add_argument('-e', dest='emnum', type=int, default=5, 
+        help='Number of iterations of EM algorithm, default=5')
+    parser.add_argument('-a', dest='Alpha', type=float, default=0.95, 
+        help='Parameter alpha, used in rank function, default=0.95')
+    parser.add_argument('-r', dest='Rank', type=int, default=1, 
+        help='Choose a rank function, 0:SimpleRanking and 1:AuthorityRanking, default=1')
+
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    # 解析命令行参数
+    args = parse_arg()
+    K = args.clusternum
+    alpha = args.Alpha
+    iter_num = args.iternum    # 迭代次数
+    now_iter = 0
+    rankfunc = args.Rank
+    if rankfunc == 1:
+        RankAlgorithm = AuthorityRanking    # AuthorityRanking
+    else:
+        RankAlgorithm = SimpleRanking
+
+    # 多进程
+    manager = multiprocessing.Manager()
+            
+    '''获取几种网络'''
+    print("Build the net of authors and conferences ...")
+    CAnet, ACnet, AAnet = BuildNet('data/data.txt')
+
+    '''
+    开始计时
+    '''
+    main_start = time.time()
+
+    '''初始化类别'''
+    print("Init the cluster ...")
+    Cluster = init_cluster(CAnet, K)
+
+    rank_confer= manager.list([{} for k in range(K)])    # 加上 manager.list，可以在子进程修改全局变量
+    rank_author= manager.list([{} for k in range(K)])
+
+    '''
+    开始迭代算法
+    '''
+    while now_iter < iter_num:
+        # 检查是否有类为空
+        if not check_cluster(Cluster):
+            print("Some cluster has been reduced to ZERO, exit ...")
+            now_iter = 0
+            Cluster = init_cluster(CAnet, K)
+        else:
+            now_iter += 1
+        # 创建进程池
+        pool = multiprocessing.Pool(processes=K)
+
+        print("********************第{}次迭代：********************".format(now_iter))
+        print("start ranking ...")
+            
+        process = []    # 进程列表
+        for k in range(K):
+            # 用分好的类别计算相对排名
+            pool.apply_async(poolrank, (rank_confer, rank_author, k))
+
+        pool.close()
+        pool.join()
+
+        # 将数据恢复成正常(单进程)模式
+        rank_confer = list(rank_confer)
+        rank_author = list(rank_author)
+            
+        print("start EM algorithm ...")
+        pai,pz = EM(CAnet, rank_confer, rank_author, Cluster, K, iternum=5)
+        print("adjust the cluster ...")
+        Cluster = adjust_class(CAnet, Cluster, pai, K)
+
+    '''
+    结束计时
+    '''
+    main_end = time.time()
+    print("**********************************************************************")
+    print("Time of RankCLus Algorithm using AuthorityRanking is {} s".format(main_end-main_start))
+    print("**********************************************************************")
+
+    '''
+    根据最后的分类再计算一次排名，得出排名最靠前的10个
+    '''
+    inrank_confer = [{} for k in range(K)]
+    for k in range(K):
+        print("********************************************")
+        print("start ranking of cluster {}".format(k))
+        # 用分好的类别计算相对排名
+        if RankAlgorithm == SimpleRanking:
+            inrank_confer[k], rank_author[k], rank_confer[k] = RankAlgorithm(CAnet, ACnet, Cluster[k])
+        else:
+            inrank_confer[k], rank_author[k], rank_confer[k] = RankAlgorithm(CAnet, ACnet, AAnet, Cluster[k], alpha)
+
+        # heapq堆算法，取最大的10个依次输出
+        print("Conference information ...")
+        for confer in heapq.nlargest(10, inrank_confer[k].items(), lambda x: x[1]):
+            print(str(confer[0]), end='\t')
+        print('\n' * 3)
+        print("Author information ...")
+        for author in heapq.nlargest(10, rank_author[k].items(), lambda x: x[1]):
+            print(str(author[0]), end='\t')
+        print('\n' * 3)
